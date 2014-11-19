759df52039f38a70e1de1412383e164e520411f5
ntddk <ntinternal@gmail.com>
2014-11-19 15:13:00 +0900

    Add configure.sh

---
+++ configure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PROJECT=$1
+
+if [ $# != 1 ]; then
+    echo "you must specify a project name" 1>&2
+    exit 0
+
+./configure --target-list=i386-softmmu --proj-name=${PROJECT} --prefix=$(pwd)/install --disable-gcc-check
